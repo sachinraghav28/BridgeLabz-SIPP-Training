2690925965468c6ebc0590812ceadb8767fd7b4d
Sachin Raghav <115388239+sachinraghav28@users.noreply.github.com>
2025-06-16 12:04:24 +0530

    Successfully added

--- Areaofacircle.java
+++
@@ -1,8 +0,0 @@
-public class Areaofacircle {
-    public static void main(String[] args) {
-        double r = 7.0; 
-        double area = Math.PI * r * r;
-        System.out.println("The area of the circle" + area);
-    }
-    
-}
