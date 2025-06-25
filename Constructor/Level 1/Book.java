a0d1b45f42498861b510a484633ecde48b696884
Sachin Raghav <115388239+sachinraghav28@users.noreply.github.com>
2025-06-25 08:56:50 +0530

    [Sachin] Constructor complete

--- Constructor/Level 1/Book.java
+++ Constructor/Level 1/Book.java
@@ -7,6 +7,7 @@ public class Book {
         this.author = "Unknown";
         this.price = 0.0;
     }
+
     public Book(String title, String author, double price) {
         this.title = title;
         this.author = author;
